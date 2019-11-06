0860cf8662fc653b4042aa33167e456fd31ab993
naflaa11 <53143354+naflaa11@users.noreply.github.com>
2019-11-06 13:47:14 +0300

    Add files via upload

--- crm_notification/crm_notification/__manifest__.py
+++ crm_notification/crm_notification/__manifest__.py
@@ -3,28 +3,27 @@
     'name': "crm_notification",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       CRM Notification upon Lead,Qualified, Quotation, Won/Lost""",
 
-    'description': """
+    'description': """,
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Telenoc",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base','crm','sale','sale_management','mail'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
--- crm_notification/crm_notification/models/models.py
+++ crm_notification/crm_notification/models/models.py
@@ -11,41 +11,19 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 class Lead(models.Model):
     _inherit = "crm.lead"
 
+    #
+    # hunain=fields.Selection([('new', 'NEW'),
+    #     ('qualified', 'Qualified'),
+    #     ('quotation', 'Quotation'),
+    #     ('won', 'Won'),
+    #     ], 'Status')
+
 
     @api.onchange('stage_id')
     def _onchange_stage_id(self):
         values = self._onchange_stage_id_values(self.stage_id.id)
         self.update(values)
-        # print(self._origin.id)
         groupObj = self.env['res.groups'].search([('name', '=', "User: Own Documents Only")])
-
-        if self.stage_id.name == 'Qualified':
-
-            act_type_xmlid = 'mail.mail_activity_data_todo'
-            date_deadline = datetime.now().strftime('%Y-%B-%d')
-            summary = 'Qualified Lead Notification'
-            note = 'Your Lead is Qualified, Please take follow-up.'
-
-            if act_type_xmlid:
-                activity_type = self.sudo().env.ref(act_type_xmlid)
-
-            model_id = self.env['ir.model']._get(self._name).id
-
-            create_vals = {
-                'activity_type_id': activity_type.id,
-                'summary': summary or activity_type.summary,
-                'automated': True,
-                'note': note,
-                'date_deadline': date_deadline,
-                'res_model_id': model_id,
-                'res_id': self._origin.id,
-                'user_id': self.user_id.id,
-
-            }
-            # print("Hello 2")
-            activities = self.env['mail.activity'].create(create_vals)
-
-            # print(create_vals)
 
         for i in groupObj.users:
             userObj = self.env['res.users'].search([('id', '=', i.id)])
@@ -53,8 +31,10 @@ class Lead(models.Model):
 
             if self.stage_id.name == 'Won':
 
+
+
                 act_type_xmlid = 'mail.mail_activity_data_todo'
-                date_deadline = datetime.now().strftime('%Y-%B-%d')
+                date_deadline = datetime.now().strftime('%Y-%m-%d')
                 summary = 'Won Lead Notification'
                 note = 'This Lead is Won, Please take follow-up.'
 
@@ -74,21 +54,17 @@ class Lead(models.Model):
                     'user_id': userObj.id,
 
                 }
-                # print("Hello 2")
+
                 activities = self.env['mail.activity'].create(create_vals)
 
-                # print(create_vals)
+            if self.stage_id.name == 'Quotation':
 
-        for i in groupObj.users:
-            userObj = self.env['res.users'].search([('id', '=', i.id)])
-            # print(userObj.name)
 
-            if self.stage_id.name == 'Qualified':
 
                 act_type_xmlid = 'mail.mail_activity_data_todo'
-                date_deadline = datetime.now().strftime('%Y-%B-%d')
-                summary = 'Qualified Lead Notification'
-                note = 'Your Lead is Qualified, Please take follow-up.'
+                date_deadline = datetime.now().strftime('%Y-%m-%d')
+                summary = 'Quoation Notification'
+                note = 'Your Lead Quotation is created, Please take follow-up.'
 
                 if act_type_xmlid:
                     activity_type = self.sudo().env.ref(act_type_xmlid)
@@ -106,10 +82,73 @@ class Lead(models.Model):
                     'user_id': userObj.id,
 
                 }
-                # print("Hello 2")
                 activities = self.env['mail.activity'].create(create_vals)
 
-                # print(create_vals)
+            if self.stage_id.name == 'Qualified':
+
+                act_type_xmlid = 'mail.mail_activity_data_todo'
+                date_deadline = datetime.now().strftime('%Y-%m-%d')
+                summary = 'Qualified Lead Notification'
+                note = 'Your Lead is Qualified, Please take follow-up.'
+
+                if act_type_xmlid:
+                    activity_type = self.sudo().env.ref(act_type_xmlid)
+
+                model_id = self.env['ir.model']._get(self._name).id
+
+                create_vals = {
+                    'activity_type_id': activity_type.id,
+                    'summary': summary or activity_type.summary,
+                    'automated': True,
+                    'note': note,
+                    'date_deadline': date_deadline,
+                    'res_model_id': model_id,
+                    'res_id': self._origin.id,
+                    'user_id': self.user_id.id,
+
+                }
+
+                activities = self.env['mail.activity'].create(create_vals)
+
+            else:
+                pass
+
+    @api.model
+    def create(self, values):
+        company = super(Lead, self).create(values)
+        company._create_function()
+        return company
+
+    @api.multi
+    def _create_function(self):
+
+            groupObj = self.env['res.groups'].search([('name', '=', "User: Own Documents Only")])
+
+            for i in groupObj.users:
+                userObj = self.env['res.users'].search([('id', '=', i.id)])
+                act_type_xmlid = 'mail.mail_activity_data_todo'
+                date_deadline = datetime.now().strftime('%Y-%m-%d')
+                summary = 'New Lead Notification'
+                note = 'New Lead is created, Please take follow-up.'
+
+                if act_type_xmlid:
+                    activity_type = self.sudo().env.ref(act_type_xmlid)
+
+                model_id = self.env['ir.model']._get(self._name).id
+
+                create_vals = {
+                    'activity_type_id': activity_type.id,
+                    'summary': summary or activity_type.summary,
+                    'automated': True,
+                    'note': note,
+                    'date_deadline': date_deadline,
+                    'res_model_id': model_id,
+                    'res_id': self.id,
+                    'user_id': userObj.id,
+
+                }
+
+                activities = self.env['mail.activity'].create(create_vals)
 
 
 
@@ -122,16 +161,11 @@ class Lead(models.Model):
 
         groupObj = self.env['res.groups'].search([('name', '=', "User: Own Documents Only")])
 
-        print(self.id)
-
         for i in groupObj.users:
             userObj = self.env['res.users'].search([('id', '=', i.id)])
 
-
-
-
             act_type_xmlid = 'mail.mail_activity_data_todo'
-            date_deadline = datetime.now().strftime('%Y-%B-%d')
+            date_deadline = datetime.now().strftime('%Y-%m-%d')
             summary = 'Lost Lead Notification'
             note = 'Your Lead is Lost, Please take follow-up.'
 
@@ -139,7 +173,6 @@ class Lead(models.Model):
                 activity_type = self.sudo().env.ref(act_type_xmlid)
 
             model_id = self.env['ir.model']._get(self._name).id
-            print(model_id)
 
             create_vals = {
                 'activity_type_id': activity_type.id,
@@ -154,57 +187,45 @@ class Lead(models.Model):
             }
             activities = self.env['mail.activity'].create(create_vals)
 
-    # @api.model
-    # def group_b_users(self):
-    #
-    #     groupObj = self.env['res.groups'].search([('name', '=', "User: Own Documents Only")])
-    #
-    #     print(self.id)
-    #
-    #     for i in groupObj.users:
-    #         userObj = self.env['res.users'].search([('id', '=', i.id)])
-    #
-    #         user_id=i.id
-
-
-class SaleOrder_lead(models.Model):
-    _inherit = "sale.order"
-
-    @api.multi
-    def action_confirm(self, vals):
-
-        groupObj = self.env['res.groups'].search([('name', '=', "User: Own Documents Only")])
-
-
-
-        for i in groupObj.users:
-            print(i.name)
-            userObj = self.env['res.users'].search([('id', '=', i.id)])
-
-            act_type_xmlid = 'mail.mail_activity_data_todo'
-            date_deadline = datetime.now().strftime('%Y-%B-%d')
-            summary = 'Quotation Lead Notification'
-            note = 'Quotation is sent, Please take follow-up.'
-
-            if act_type_xmlid:
-                activity_type = self.sudo().env.ref(act_type_xmlid)
-
-            model_id = self.env['ir.model']._get(self._name).id
-
-            create_vals = {
-                'activity_type_id': activity_type.id,
-                'summary': summary or activity_type.summary,
-                'automated': True,
-                'note': note,
-                'date_deadline': date_deadline,
-                'res_model_id': model_id,
-                'res_id': self.id,
-                'user_id': userObj.id,
-
-            }
-            activities = self.env['mail.activity'].create(create_vals)
-
-
-
-
-
+#
+# class SaleOrder_lead(models.Model):
+#     _inherit = "sale.order"
+#
+#     @api.multi
+#     def action_confirm(self):
+#
+#         groupObj = self.env['res.groups'].search([('name', '=', "User: Own Documents Only")])
+#
+#
+#
+#         for i in groupObj.users:
+#             print(i.name)
+#             userObj = self.env['res.users'].search([('id', '=', i.id)])
+#
+#             act_type_xmlid = 'mail.mail_activity_data_todo'
+#             date_deadline = datetime.now().strftime('%Y-%B-%d')
+#             summary = 'Quotation Lead Notification'
+#             note = 'Quotation is sent, Please take follow-up.'
+#
+#             if act_type_xmlid:
+#                 activity_type = self.sudo().env.ref(act_type_xmlid)
+#
+#             model_id = self.env['ir.model']._get(self._name).id
+#
+#             create_vals = {
+#                 'activity_type_id': activity_type.id,
+#                 'summary': summary or activity_type.summary,
+#                 'automated': True,
+#                 'note': note,
+#                 'date_deadline': date_deadline,
+#                 'res_model_id': model_id,
+#                 'res_id': self.id,
+#                 'user_id': userObj.id,
+#
+#             }
+#             activities = self.env['mail.activity'].create(create_vals)
+#
+#
+#
+#
+#
